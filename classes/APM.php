67a4ee1329c19c55058d0063a99486019d9bfcc1
Matheus Evangelista <evangelista.developer@gmail.com>
2020-04-09 21:48:00 -0300

    add files

---
+++ classes/APM.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * Class APM
+ */
+
+class APM {
+
+  protected $agent = null;
+  protected $transaction = null;
+
+  public function __construct() {
+
+    $config = [
+      'appName'     => APM_APPNAME,
+      'appVersion'  => APM_APPVERSION,
+      'serverUrl'   => APM_SERVERURL,
+      'secretToken' => APM_SECRETTOKEN,
+      'active'      => APM_ACTIVE,
+      'hostname'    => gethostname(),
+      'env'         => ['DOCUMENT_ROOT', 'REMOTE_ADDR', 'REMOTE_USER', 'APM_ENVIRONMENT']
+    ];
+
+    $this->agent = new \PhilKra\Agent($config);
+
+    $this->create_transaction($_SERVER['REQUEST_URI']);
+
+    add_action('wp_loaded', array( $this, 'finish_transaction' ) );
+
+  }
+
+  public function create_transaction($url){
+
+    $mem = $this->getServerMemoryUsage(false);
+
+    $this->agent->putEvent($this->agent->factory()->newMetricset([
+      'system.cpu.total.norm.pct' => min(sys_getloadavg()[0]/100, 1),
+      'system.memory.total' => $mem['total'],
+      'system.memory.actual.free' => $mem['free']
+    ]));
+
+    $this->transaction = $this->agent->startTransaction('GET '.$url);
+
+  }
+
+  public function finish_transaction(){
+
+    $this->agent->stopTransaction($this->transaction->getTransactionName());
+
+  }
+
+  function getServerMemoryUsage($getPercentage=true){
+
+    $memoryTotal = null;
+    $memoryFree = null;
+
+    if (stristr(PHP_OS, "win")) {
+        // Get total physical memory (this is in bytes)
+        $cmd = "wmic ComputerSystem get TotalPhysicalMemory";
+        @exec($cmd, $outputTotalPhysicalMemory);
+
+        // Get free physical memory (this is in kibibytes!)
+        $cmd = "wmic OS get FreePhysicalMemory";
+        @exec($cmd, $outputFreePhysicalMemory);
+
+        if ($outputTotalPhysicalMemory && $outputFreePhysicalMemory) {
+            // Find total value
+            foreach ($outputTotalPhysicalMemory as $line) {
+                if ($line && preg_match("/^[0-9]+\$/", $line)) {
+                    $memoryTotal = $line;
+                    break;
+                }
+            }
+
+            // Find free value
+            foreach ($outputFreePhysicalMemory as $line) {
+                if ($line && preg_match("/^[0-9]+\$/", $line)) {
+                    $memoryFree = $line;
+                    $memoryFree *= 1024;  // convert from kibibytes to bytes
+                    break;
+                }
+            }
+        }
+    } else {
+
+        if (is_readable("/proc/meminfo")){
+
+            $stats = @file_get_contents("/proc/meminfo");
+
+            if ($stats !== false) {
+                // Separate lines
+                $stats = str_replace(array("\r\n", "\n\r", "\r"), "\n", $stats);
+                $stats = explode("\n", $stats);
+
+                // Separate values and find correct lines for total and free mem
+                foreach ($stats as $statLine) {
+                    $statLineData = explode(":", trim($statLine));
+
+                    //
+                    // Extract size (TODO: It seems that (at least) the two values for total and free memory have the unit "kB" always. Is this correct?
+                    //
+
+                    // Total memory
+                    if (count($statLineData) == 2 && trim($statLineData[0]) == "MemTotal") {
+                        $memoryTotal = trim($statLineData[1]);
+                        $memoryTotal = explode(" ", $memoryTotal);
+                        $memoryTotal = $memoryTotal[0];
+                        $memoryTotal *= 1024;  // convert from kibibytes to bytes
+                    }
+
+                    // Free memory
+                    if (count($statLineData) == 2 && trim($statLineData[0]) == "MemFree") {
+                        $memoryFree = trim($statLineData[1]);
+                        $memoryFree = explode(" ", $memoryFree);
+                        $memoryFree = $memoryFree[0];
+                        $memoryFree *= 1024;  // convert from kibibytes to bytes
+                    }
+                }
+            }
+        }
+    }
+
+    if (is_null($memoryTotal) || is_null($memoryFree)) {
+        return null;
+    } else {
+        if ($getPercentage) {
+            return (100 - ($memoryFree * 100 / $memoryTotal));
+        } else {
+            return array(
+                "total" => $memoryTotal,
+                "free" => $memoryFree,
+            );
+        }
+    }
+  }
+
+}
---
+++ wordpress-apm-agent-elasticsearch.php
@@ -0,0 +1,48 @@
+<?php
+
+  /*
+    * Plugin Name: Wordpress APM Agent Elasticsearch
+    * Plugin URI: https://github.com/matheusevangelista/Wordpress-APM-Agent-Elasticsearch
+    * Description: A (unofficial) WordPress plugin to send data to APM agent of Elasticsarch.
+    * Version: 1.0.0
+    * Author: Matheus Evangelista
+    * Author URI: https://github.com/matheusevangelista
+    * License: MIT
+  */
+
+  require 'vendor/autoload.php';
+
+  if ( ! defined( 'WP_APM_PLUGIN_PATH' ) ) {
+    define( 'WP_APM_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+  }
+
+  /**
+   * Checking vars
+   */
+
+  if ( ! defined( 'APM_APPNAME' ) ) {
+    define( 'APM_APPNAME', 'app-name' );
+  }
+
+  if ( ! defined( 'APM_APPVERSION' ) ) {
+    define( 'APM_APPVERSION', '1.0.0' );
+  }
+
+  if ( ! defined( 'APM_SERVERURL' ) ) {
+    define( 'APM_SERVERURL', 'http://apm-server/8200' );
+  }
+
+  if ( ! defined( 'APM_SECRETTOKEN' ) ) {
+    define( 'APM_SECRETTOKEN', '' );
+  }
+
+  if ( ! defined( 'APM_ACTIVE' ) ) {
+    define( 'APM_ACTIVE', true );
+  }
+
+  require_once( WP_APM_PLUGIN_PATH . 'classes/APM.php' );
+
+  new APM();
+
+
+?>
